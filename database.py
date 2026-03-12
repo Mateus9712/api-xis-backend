b5c7294788f3f6e2531609fb3058447612030d0b
Mateus9712 <MATEUSPS9712@GMAIL.COM>
2026-03-12 20:09:35 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Cria o arquivo do banco de dados na sua pasta
-SQLALCHEMY_DATABASE_URL = "sqlite:///./banco_xis.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./RaizesdoNordeste.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
